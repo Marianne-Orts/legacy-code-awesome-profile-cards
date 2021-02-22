1883ca80473da631b240ab5941c2b9e30c2604db
mariavirum <mariavirum@gmail.com>
2021-02-22 15:37:02 +0100

    Add changes

--- src/components/CollapsableList.js
+++ src/components/CollapsableList.js
@@ -14,18 +14,18 @@ function CollapsableList() {
 
 export default CollapsableList;
 
-/* <div className="dropdown js-dropdown-header dropdownshare-btn">
-          <div className="dropdown__title">
-            <span className="dropdown__icon dropdown__icon--left fas fa-share-alt"></span>
-            <h2 className="dropdown__text">Comparte</h2>
-          </div>
-          <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
-        </div> */
-
 /* <div className="dropdown js-dropdown-header ">
           <div className="dropdown__title">
             <span className="dropdown__icon dropdown__icon--left far fa-keyboard dropdown__icon "></span>
             <h2 className="dropdown__text">Rellena</h2>
+          </div>
+          <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
+        </div> */
+
+/* <div className="dropdown js-dropdown-header dropdownshare-btn">
+          <div className="dropdown__title">
+            <span className="dropdown__icon dropdown__icon--left fas fa-share-alt"></span>
+            <h2 className="dropdown__text">Comparte</h2>
           </div>
           <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
         </div> */
