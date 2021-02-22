e6f94c1dd05421bbb8dc4ba0eac03d5dba2c1000
Marianne-Orts <ma.orts.ma@gmail.com>
2021-02-22 17:32:39 +0100

    Create collapsable component and props

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,4 +1,15 @@
-// function Collapsable(){
-//     return()
-// }
-// export default Collapsable;
+function Collapsable(props) {
+  return (
+    <div className="dropdown js-dropdown-header" shareBtn={props.shareBtn}>
+      <div className="dropdown__title">
+        <span
+          className="dropdown__icon dropdown__icon--left"
+          iconCollapsable={props.iconCollapsable}
+        ></span>
+        <h2 className="dropdown__text">{props.title}</h2>
+      </div>
+      <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
+    </div>
+  );
+}
+export default Collapsable;
--- src/components/CollapsableList.js
+++ src/components/CollapsableList.js
@@ -2,30 +2,19 @@ import Collapsable from "./Collapsable";
 
 function CollapsableList() {
   return (
-    <div className="dropdown js-dropdown-header ">
-      <div className="dropdown__title">
-        <span className="dropdown__icon dropdown__icon--left far fa-object-ungroup"></span>
-        <h2 className="dropdown__text">Diseña</h2>
-      </div>
-      <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
-    </div>
+    <>
+      <Collapsable iconCollapsable={"far fa-object-ungroup"} title={"Diseña"} />
+      <Collapsable
+        iconCollapsable={"far fa-keyboard dropdown__icon"}
+        title={"Rellena"}
+      />
+      <Collapsable
+        shareBtn={"dropdownshare-btn"}
+        iconCollapsable={"fas fa-share-alt"}
+        title={"Comparte"}
+      />
+    </>
   );
 }
 
 export default CollapsableList;
-
-/* <div className="dropdown js-dropdown-header ">
-          <div className="dropdown__title">
-            <span className="dropdown__icon dropdown__icon--left far fa-keyboard dropdown__icon "></span>
-            <h2 className="dropdown__text">Rellena</h2>
-          </div>
-          <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
-        </div> */
-
-/* <div className="dropdown js-dropdown-header dropdownshare-btn">
-          <div className="dropdown__title">
-            <span className="dropdown__icon dropdown__icon--left fas fa-share-alt"></span>
-            <h2 className="dropdown__text">Comparte</h2>
-          </div>
-          <i className="fa fa-chevron-down js-arrow dropdown__arrow"></i>
-        </div> */
