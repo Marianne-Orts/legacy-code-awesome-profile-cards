54ba4cd2f3b724a1c1baeae4612318e70503fffa
Marianne-Orts <ma.orts.ma@gmail.com>
2021-03-02 16:29:45 +0100

    Test new components

--- src/components/GetAvatar.js
+++ src/components/GetAvatar.js
@@ -1,25 +1,77 @@
-function GetAvatar() {
-  return (
-    <div className="fill__container">
-      <label className="form__label" for="photo">
-        Imagen de perfil
-      </label>
-      <div className="image-form__container">
-        <div className="button">
-          <label className="button__form js-buttonForm" for="photo">
-            Añadir imagen
-          </label>
+/* import React, { Component } from "react";
+import "./App.scss";
+import PropTypes from "prop-types";
+
+class GetAvatar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.fr = new FileReader();
+    this.myFileField = React.createRef();
+
+    this.handleFilePicker = this.handleFilePicker.bind(this);
+    this.uploadImage = this.uploadImage.bind(this);
+    this.getImage = this.getImage.bind(this);
+  }
+
+  handleFilePicker() {
+    this.myFileField.current.click();
+  }
+
+  uploadImage(e) {
+    const myFile = e.currentTarget.files[0];
+    this.fr.addEventListener("load", this.getImage);
+    this.fr.readAsDataURL(myFile);
+  }
+
+  getImage() {
+    const image = this.fr.result;
+    this.props.updateAvatar(image);
+  }
+
+  getPreview(isDefault, image) {
+    return !isDefault ? { backgroundImage: `url(${image})` } : {};
+  }
+
+  render() {
+    const { isAvatarDefault, avatar } = this.props;
+    return (
+      <div className="fill__container">
+        <label className="form__label" for="photo">
+          Imagen de perfil
+        </label>
+        <div className="image-form__container">
+          <div className="button">
+            <label
+              className="button__form js-buttonForm"
+              onClick={this.handleFilePicker}
+              for="photo"
+            >
+              Añadir imagen
+            </label>
+          </div>
+          <input
+            className="form__input button-image js-inputPhoto"
+            type="file"
+            value=""
+            id="photo"
+            ref={this.myFileField}
+            onChange={this.uploadImage}
+          />
+          <div
+            className="small-image__container smallphoto js-smallPhoto"
+            style={this.getPreview(isAvatarDefault, avatar)}
+          ></div>
         </div>
-        <input
-          className="form__input button-image js-inputPhoto"
-          type="file"
-          value=""
-          id="photo"
-        />
-        <div className="small-image__container smallphoto js-smallPhoto"></div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default GetAvatar;
+GetAvatar.propTypes = {
+  isAvatarDefault: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired,
+  updateAvatar: PropTypes.func.isRequired,
+};
+
+export default GetAvatar; */
---
+++ src/services/SendDataToApi.js
@@ -0,0 +1,32 @@
+/* const SendDataToApi = () => {
+    const urlServe = "https://awesome-profile-cards.herokuapp.com/card";
+    const data = getUserData();
+    return fetch  
+    (urlServe, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      linksContainer.classList.remove("share__hidden");
+      if (data.success === true) {
+        textUrl.innerHTML = "La tarjeta ha sido creada:";
+        linkUrl.innerHTML = data.cardURL;
+        linkUrl.href = data.cardURL;
+        const urlTwitter = data.cardURL;
+        twitterUrl.href = `https://twitter.com/intent/tweet?url=Échale%20un%20vistazo%20a%20mi%20tarjeta%20virtual%20${urlTwitter}`;
+        twitterButton.classList.remove("twitterhidden");
+      } else {
+        textUrl.innerHTML =
+          "Ups! No se ha podido crear tu tarjeta. Rellena todos tus datos.";
+        linkUrl.href = "";
+        linkUrl.innerHTML = "";
+        twitterButton.classList.add("twitterhidden");
+      }
+    });
+ */
+
+/* shareBtn.classList.add("button-backgroundcolor"); */
