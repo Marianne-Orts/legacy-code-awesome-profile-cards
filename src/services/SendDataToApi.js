08d4d7ea2a7cd0e5849cb9cdd53c9a9450251e94
Marianne-Orts <ma.orts.ma@gmail.com>
2021-03-08 22:02:58 +0100

    Eliminate version 0 references

--- src/services/SendDataToApi.js
+++ src/services/SendDataToApi.js
@@ -8,24 +8,6 @@ const sendDataToApi = (data) => {
       "content-type": "application/json",
     },
   }).then((response) => response.json());
-  /* .then((data) => {
-      linksContainer.classList.remove("share__hidden");
-      if (data.success === true) {
-        textUrl.innerHTML = "La tarjeta ha sido creada:";
-        linkUrl.innerHTML = data.cardURL;
-        linkUrl.href = data.cardURL;
-        const urlTwitter = data.cardURL;
-        twitterUrl.href = `https://twitter.com/intent/tweet?url=Échale%20un%20vistazo%20a%20mi%20tarjeta%20virtual%20${urlTwitter}`;
-        twitterButton.classList.remove("twitterhidden");
-      } else {
-        textUrl.innerHTML =
-          "Ups! No se ha podido crear tu tarjeta. Rellena todos tus datos.";
-        linkUrl.href = "";
-        linkUrl.innerHTML = "";
-        twitterButton.classList.add("twitterhidden");
-      }
-    }); 
-    }); */
 };
 
 export default sendDataToApi;
